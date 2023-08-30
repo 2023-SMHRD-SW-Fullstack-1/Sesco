34e84066d2981ac8d74761abb6f55d6d5031c119
천현민 <poppy3672@naver.com>
2023-08-30 14:57:30 +0900

    react 초기 세팅

---
+++ sescoclient/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './common/Header';
+import Footer from './common/Footer';
+import Main from './page/Main';
+import Tip from './page/ParentingTip/Tip';
+import Note from './page/ParentingNote/Note';
+import Gallery from './page/ParentingGallery/Gallery';
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Main/>}></Route>
+      <Route path="/gallery" element={<Gallery/>}></Route>
+      <Route path="/note" element={<Note/>}></Route>
+      <Route path="/tip" element={<Tip/>}></Route>
+    </Routes>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
