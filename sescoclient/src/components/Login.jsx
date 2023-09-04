6e0a72b46e574b8654bd7ec94f4f026ac6537234
천현민 <poppy3672@naver.com>
2023-09-04 11:47:38 +0900

    login

--- sescoclient/src/components/Login.jsx
+++ sescoclient/src/components/Login.jsx
@@ -4,13 +4,9 @@ const Login = () => {
     const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
     const REDIRECT_URI = 
     "http://localhost:3000/login/oauth2/callback/kakao"
-    const loginHandler = () => {
-        window.location.href = KAKAO_AUTH_URI; // url 주소 변경
-      };
+    
   return (
-    <div>
-        
-    </div>
+    <div>Login</div>
   )
 }
 
