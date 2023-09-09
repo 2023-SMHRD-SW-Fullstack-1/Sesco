a69a768dcf49e684a372efb3d380a56cc963b5e7
천현민 <poppy3672@naver.com>
2023-09-09 19:47:23 +0900

    diary

---
+++ SescoServer/src/main/java/com/smhrd/sesco/controller/DiaryController.java
@@ -0,0 +1,12 @@
+package com.smhrd.sesco.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@CrossOrigin(origins = "http://localhost:3000")
+public class DiaryController {
+	//뭐 해야되지
+	
+
+}
