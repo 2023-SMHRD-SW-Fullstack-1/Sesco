7965267d0eabd17c9d46791f1e30083ea5de60b9
이선아 <asha.sun.99@gmail.com>
2023-09-18 10:47:17 +0900

    Update Note

--- sescoclient/src/page/ParentingNote/Note.jsx
+++ sescoclient/src/page/ParentingNote/Note.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import NoteList from './components/NoteList'
 import noteBackground from './noteImg/noteBackground.png'
 import noteFind from './noteImg/noteFind.png'
@@ -7,309 +7,269 @@ import './note.css';
 import Diarycopy from './components/Diarycopy'
 
 const Note = () => {
-  //태그 검색
-  const [searchTag, setSearchTag] = useState("");
-  //아이 선택
-  const [kidSelect, setKidSelect] = useState("모든 아이")
-  // kid_seq 값을 배열로 저장하는 상태 변수 추가 
-  const [getMoveKidSeq, setGetMoveKidSeq] = useState([]);
 
-  //수첩
-  const [notes, setNotes] = useState([]);
-  //수첩 표시
-  const [notesDisplay, setNotesDisplay] = useState({});
-  //수첩 선택
-  const [selectedNoteSeq, setSelectedNoteSeq] = useState(null);
+  
 
-  //연도 선택
-  const [selectedNoteYear, setSelectedNoteYear] = useState(null);
-
-
-  //사용자 닉네임
-  const userNick = sessionStorage.getItem("user_nick");
+  //세션에서 로그인한 유저 아이디/닉네임 정보가져오기
   const userId = sessionStorage.getItem("user_id");
+  const userNick = sessionStorage.getItem("user_nick");
+
+  //세션에 프로필에서 가져온 kidSeq값이 있는지 확인 
   const kidSeq = sessionStorage.getItem("kid_seq")
-  console.log("사용자 닉네임 : ", userNick)
-  console.log("사용자 Id :", userId)
-  //아이 정보
-  const [kids, setKids] = useState([]);
-  console.log("아이 정보 : ", kids)
-  const [isNoteCreated, setIsNoteCreated] = useState(false);
+
+  
+
+///-------------------------Note start---------------------//
+
+//수첩 목록 저장
+const [notes, setNotes] = useState([]);
+
+//선택한 수첩의 noteSeq값 저장
+const [selectedNoteSeq, setSelectedNoteSeq] = useState(null);
+
+//노트 불러오기 (다시)
+useEffect(() => {
+  const getNotes = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8081/sesco/note/${userId}`);
+      setNotes(response.data);
+      console.log("노트 불러오기", response.data);
+    } catch (e) {
+      console.error("노트 불러오기 실패 : ", e);
+    }
+  };
+  getNotes();
+}, []);
 
 
-  //노트 불러오기
-  useEffect(() => {
-    const getNotes = async () => {
-      try {
-        const response = await axios.get(`http://localhost:8081/sesco/note/${userId}`);
-        setNotes(response.data);
-        console.log("노트 불러오기", response.data);
-      } catch (e) {
-        console.error("노트 불러오기 실패 : ", e);
-      }
-    };
-    getNotes();
-  }, []);
+
+//노트 클릭시
+const handleNoteClick = async (note_seq) => {
+
+  try {
+    // notes에서 선택한 note_seq 값과 일치하는 노트 찾기
+    const selectedNote = notes.filter((note)=>note.note_seq === note_seq);
+
+    if (selectedNote) {
+      console.log('노트클릭시, note_seq, selectedNote:', note_seq, selectedNote);
+
+      // 다이어리 컴포넌트에 전달할 객체 생성
+      const noteData = {
+        kidSeq: kidSelect,
+        ...selectedNote
+      };
+
+      console.log("다이어리 컴포넌트에 전달 : ", noteData)
+    }
+  } catch (error) {
+    console.error('노트 상세 정보 가져오기 실패:', error);
+  }
+
+  setSelectedNoteSeq(note_seq);
+
+  // 만약 이미 선택된 노트를 다시 클릭했다면
+  if (selectedNoteSeq === note_seq) {
+    setSelectedNoteSeq(null);
+  } else {
+    setSelectedNoteSeq(note_seq);
+
+  }
+}
 
 
-// 사용자 아이 정보 불러오기
+
+//---------Diary start-----------//
+
+  // 일기 열었을때 닫기 버튼 클릭시
+  const handleDiaryClose = () => {
+    setSelectedNoteSeq(null);
+    console.log("일기 닫기 버튼 클릭 ")
+  };
+  
+//----------Diary end----------//
+
+
+///-------------------------Note end ----------------------//
+
+
+
+///-------------------------Kid start----------------------//
+
+ //아이 정보
+ const [kids, setKids] = useState([]);
+
+ //// 사용자 아이 정보 불러오기
 useEffect(() => {
   const getKids = async () => {
     try {
       const response = await axios.post('http://localhost:8081/sesco/kid/getkidlist', { user_id: userId })
       setKids(response.data)
       console.log("사용자 아이 불러오기 성공", response.data)
-
-      // 각 아이에 대해 노트를 생성하거나 확인
-      for (const kid of response.data) {
-        const kidNotesResponse = await axios.get(`http://localhost:8081/sesco/note/${kid.kid_seq}`);
-        const kidNotes = kidNotesResponse.data;
-        console.log("아이의 노트 정보:", kidNotes.length);
-
-        // 아이의 노트가 없으면 노트 생성
-        if (kidNotes.length === 0) {
-          console.log(`아이의 노트가 없으므로 노트 생성: ${kid.kid_name}`);
-          await axios.post('http://localhost:8081/sesco/note/createnote', kid);
-          
-          // isNoteCreated 값을 true로 설정하여 한 번만 실행되도록 함
-          setIsNoteCreated(true);
-        }
-      }
-
-      const allKidSeq = response.data.map((kid) => kid.kid_seq);
-      setGetMoveKidSeq(allKidSeq)
-      console.log("모든아이 kid_seq값 배열1 : ", allKidSeq);
     } catch (e) {
       console.error("아이 정보 불러오기 실패", e)
     }
   }
-
   // 사용자 아이 정보 불러오기 함수 호출
   getKids();
-}, [userId, setIsNoteCreated]); // 의존성 배열에 setIsNoteCreated 추가
+}, []);
 
-//노트 상태 업데이트 후 실행할 코드 추가
+//옵션 선택한 아이 저장 
+const [kidSelect, setKidSelect] = useState()
+
+//<select> useRef
+const selectRef = useRef();
+
+const defaultSelect = () => {
+  kidSeq ? selectRef.current.value = kidSeq : selectRef.current.selectedIndex = 0
+  const temp = selectRef.current.value
+  setKidSelect(temp)
+}
+
+//로드시 세션에 저장된 kid_seq값이 있는 경우에는 해당 아이로 설정 없는 경우에는 처음에있는 아이로 설정
 useEffect(() => {
-  if (isNoteCreated) { 
-    // isNoteCreated 값에 따라 추가 작업 수행
-    
-    const getUpdatedNotesByKid = async () => {
-      try{
-         // 이전에 생성한 note와 동일한 방식으로 해당 아이의 업데이트된 note를 가져옴
+  defaultSelect()
+}, []);
 
-         for(const updatedKid of kids){
-           const updatedKidNotesResponse = await axios.get(`http://localhost:8081/sesco/note/${updatedKid.kid_seq}`);
-           const updatedKidNotes = updatedKidNotesResponse.data;
-           console.log(`업데이트된 ${updatedKid.kid_name}의 note 정보`, updatedKidNotes);
-           
-           // 업데이트된 note 상태를 업데이트함 
-           setSelectedNoteSeq(updatedSelectedNoteSeq=>({
-             ...updatedSelectedNoteSeq,
-             [updatedKid.kid_seq]: updatedKidNotes,
-           }));
-         }
-       }catch(e){
-         console.error("업데이트된 note 가져오기 실패:", e);
-       }
-     };
-    
-     getUpdatedNotesByKid();
-   }
- }, [isNoteCreated, kids]);
 
-  //아이 선택했을때
-  useEffect(() => {
-    const getNotesByKid = async () => {
-      try {
-        //받아온 시퀀스 값이 있으면 해당 시퀀스 값을 설정
+//선택된 아이 설정
+const handlekidSelectChange = (e) => {
+  setKidSelect(e.target.value);
+  console.log(e.target.value)
+  //이후에 선택 시 그 아이의 수첩만 가져오게 하기
+}
 
-        if (kidSeq == null && kidSelect == null) {
-          setKidSelect("모든 아이")
-        } else if (kidSelect === "모든 아이") { // '모든 아이'가 선택된 경우
-          const reseponse = await axios.get(`http://localhost:8081/sesco/note/${userId}`);
-          setNotes(reseponse.data)
-          const allKidSeq = kids.map((kid) => kid.kid_seq);
-          console.log("출력", allKidSeq)
-          setGetMoveKidSeq(allKidSeq)
-          console.log("모든 아이 선택 : ", reseponse.data)
-        } else { // 특정 아이가 선택된 경우
-          console.log("kidSelect ", kidSelect)
-          const reseponse = await axios.post('http://localhost:8081/sesco/note/getnotelist', { kid_seq: kidSelect });
-          setNotes(reseponse.data)
-          console.log("특정 아이 선택", reseponse.data)
-          setGetMoveKidSeq(kidSelect)
-          console.log("특정 아이의 kid_seq 값: ", kidSelect)
-        }
 
-      } catch (e) {
-        console.error("노트 불러오기 실패 : ", e);
-      }
-    }
-    if (kidSelect) {
-      getNotesByKid();
-    }
-  }, [kidSelect]);
-
-  //태그 검색 이벤트
-  const handleSearchTagChange = (e) => {
-    setSearchTag(e.target.value);
-    console.log(e.target.value);
-  };
-
-  //검색 버튼 눌렀을 때
-  const handleTagSearch = async () => {
-    console.log("검색 결과는 : ", searchTag)
-    setSearchTag("")
+//선택한 아이의 노트 정보를 불러옴 (다시)
+useEffect(() => {
+  const getNotesByKid = async () => {
     try {
-      if (searchTag.trim() === "") return;
-
-      const response = await axios.get('http://localhost:8081/sesco/note/tagsearch', { params: { tag: searchTag } })
-      setNotes(response.data)
-      console.log("태그 검색 데이터 불러오기 : ", response.data)
-
-      //태그 검색 버튼 결과가 없을 때 
-      if (Object.keys(response.data).length === 0) {
-        alert("태그 검색 결과가 없습니다.😥")
-        // 모든 아이의 수첩 불러오기
-        const allNotesResponse = await axios.get('http://localhost:8081/sesco/note');
-        setNotes(allNotesResponse.data);
-
-        console.log("태그 검색 결과 없을 때 모든 수첩 데이터 불러오기 :", allNotesResponse.data);
-      }
-
+        const reseponse = await axios.get(`http://localhost:8081/sesco/note/${userId}`);
+        setNotes(reseponse.data)
+        console.log("노트 설정완료 : ", reseponse.data)
     } catch (e) {
-      console.error("태그 검색 실패 : ", e)
+      console.error("노트 불러오기 실패 : ", e);
     }
+  }
+  
+  getNotesByKid();
+}, []);
+
+///-------------------------Kid end -----------------------//
+
+
+
+///-------------------------TAG start ---------------------//
+
+//태그 검색
+const [searchTag, setSearchTag] = useState("");
+
+//태그 입력창 이벤트
+const handleSearchTagChange = (e) => {
+  setSearchTag(e.target.value);
+  console.log(e.target.value);
+};
+
+//검색 버튼 눌렀을 때 이벤트
+const handleTagSearch = async () => {
+  console.log("검색 결과는 : ", searchTag)
+  setSearchTag("")
+  try {
+    if (searchTag.trim() === "") return;
+
+    const response = await axios.get('http://localhost:8081/sesco/note/tagsearch', { params: { tag: searchTag } })
+    setNotes(response.data)
+    console.log("태그 검색 데이터 불러오기 : ", response.data)
+
+    //태그 검색 버튼 결과가 없을 때 
+    if (Object.keys(response.data).length === 0) {
+      alert("태그 검색 결과가 없습니다.😥")
+      // 모든 아이의 수첩 불러오기
+      const allNotesResponse = await axios.get('http://localhost:8081/sesco/note');
+      setNotes(allNotesResponse.data);
+
+      console.log("태그 검색 결과 없을 때 모든 수첩 데이터 불러오기 :", allNotesResponse.data);
+    }
+
+  } catch (e) {
+    console.error("태그 검색 실패 : ", e)
+  }
+  setSearchTag("")
+}
+
+// Enter 키 누를 때 검색 실행
+const handleKeyPress = (e) => {
+  if (e.key === 'Enter') {
+    handleTagSearch();
     setSearchTag("")
   }
-  // Enter 키 누를 때 검색 실행
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handleTagSearch();
-      setSearchTag("")
-    }
-  }
+}
 
-  //아이 선택 함수
-  const handlekidSelectChange = (e) => {
-    setKidSelect(e.target.value);
+console.log("notes : ", notes)
 
-    console.log(e.target.value)
-    //선택 시 그 아이의 수첩만 가져오게 하기
-  }
-
-
-  // 다이어리 컴포넌트에 전달할 수첩 객체
-  let noteData = null;
-
-  //노트 클릭시
-  const handleNoteClick = async (note_seq, year) => {
-
-    try {
-      // notes에서 선택한 note_seq 값과 일치하는 노트 찾기
-      const selectedNote = notes[year].find(note => note.note_seq === note_seq);
-
-      if (selectedNote) {
-        console.log('노트클릭시, note_seq, year, selectedNote:', note_seq, year, selectedNote);
-
-        // 다이어리 컴포넌트에 전달할 객체 생성
-        const noteData = {
-          kidSeq: kidSelect,
-          ...selectedNote
-        };
-
-        console.log("다이어리 컴포넌트에 전달 : ", noteData)
-      }
-    } catch (error) {
-      console.error('노트 상세 정보 가져오기 실패:', error);
-    }
-
-    setSelectedNoteSeq(note_seq);
-    setSelectedNoteYear(year);
-
-    // 만약 이미 선택된 노트를 다시 클릭했다면
-    if (selectedNoteSeq === note_seq && selectedNoteYear === year) {
-      setSelectedNoteSeq(null);
-      setSelectedNoteYear(null);
-    } else {
-      setSelectedNoteSeq(note_seq);
-      setSelectedNoteYear(year);
-
-    }
-  }
-
-  // 일기 열었을때 닫기 버튼 클릭시
-  const handleDiaryClose = () => {
-    setSelectedNoteSeq(null);
-    setSelectedNoteYear(null);
-    console.log("일기 닫기 버튼 클릭 ")
-  };
-
+///-------------------------TAG end ---------------------//
 
   return (
     <div className='note-container'>
-      <div className="book">
-        <img className="noteBackground" src={noteBackground} />
-        <div className="note-book-text">
-          <span className="book-span">
-            “ {userNick} ” 님의 <br />
-          </span>
-          <span className="book-span2">Book 캡슐 🧊 </span>
-        </div>
+    <div className="book">
+      <img className="noteBackground" src={noteBackground} />
+      <div className="note-book-text">
+        <span className="book-span">
+          “ {userNick} ” 님의 <br />
+        </span>
+        <span className="book-span2">Book 캡슐 🧊 </span>
       </div>
-
-      {/* 태그 검색 div */}
-
-      <div className='noteTagAndKid-container'>
-        <div className='noteTagSearch-container'>
-          <input
-            className='noteTagFind2'
-            type='search'
-            placeholder='태그 검색'
-            value={searchTag}
-            onChange={handleSearchTagChange}
-            onKeyDown={handleKeyPress} />
-          <img src={noteFind} alt='tagSearch ' className='noteTagFindIcon' onClick={handleTagSearch} />
-        </div>
-
-        {/* 아이선택 div */}
-        <div className='noteKidSelect'>
-          {/* value 값은 현재 선택한 아이 kid_seq값으로 설정 */}
-          <select className='note-myKidSelect' title="아이 선택" onChange={handlekidSelectChange} value={kidSelect}>
-            <option className='note-myKidSelectOption' value="모든 아이">모든 아이</option> {/* '모든 아이' 옵션 추가 */}
-            {kids.map((kid) => (
-              <option className='note-myKidSelectOption' value={kid.kid_seq} key={kid.kid_seq}>{kid.kid_name}</option>
-            ))}
-          </select>
-
-        </div>
-      </div>
-
-      <div className='noteList'>
-        <NoteList
-          notes={notes.slice(0, 6).map(note => ({
-            name: note.n_name,
-            startDate: note.n_s_date,
-            endDate: note.n_e_date,
-            seq: note.note_seq
-          }))}
-          onNoteClick={(note_seq) => handleNoteClick(note_seq)}
-          kidSeq={kidSelect}
-          kids = {kids}
-          allKidSeq={getMoveKidSeq}
-        />
-
-        {/** 선택된 연도와 노트 있을 경우 다이어리 표시 */}
-        {selectedNoteSeq && (
-          <div className='diary-container active'>
-            <Diarycopy kidSeq={kidSelect} noteData={noteData} />
-            <button className='diary-close' onClick={() => handleDiaryClose()}>X</button>
-          </div>
-        )}
-      </div>
-
     </div>
-  );
-};
 
-export default Note;
+    {/* 태그 검색 div */}
+
+    <div className='noteTagAndKid-container'>
+      <div className='noteTagSearch-container'>
+        <input
+          className='noteTagFind2'
+          type='search'
+          placeholder='태그 검색'
+          value={searchTag}
+          onChange={handleSearchTagChange}
+          onKeyDown={handleKeyPress} />
+        <img src={noteFind} alt='tagSearch ' className='noteTagFindIcon' onClick={handleTagSearch} />
+      </div>
+
+      {/* 아이선택 div */}
+      <div className='noteKidSelect'>
+        {/* value 값은 현재 선택한 아이 kid_seq값으로 설정 */}
+        <select ref={selectRef} className='note-myKidSelect' title="아이 선택" onChange={handlekidSelectChange} value={kidSelect}>
+          {kids.map((kid) => (
+            <option className='note-myKidSelectOption' value={kid.kid_seq} key={kid.kid_seq}>{kid.kid_name}</option>
+          ))}
+        </select>
+
+      </div>
+    </div>
+
+    <div className='noteList'>
+      <NoteList
+        notes={notes.slice(0, 6).map(note => ({
+          n_name: note.n_name,
+          n_s_date: note.n_s_date,
+          n_e_date: note.n_e_date,
+          note_seq: note.note_seq
+        }))}
+        onNoteClick={(note_seq) => handleNoteClick(note_seq)}
+        kidSeq={kidSelect}
+        kids={kids}
+      />
+      
+      {/** 선택된 연도와 노트 있을 경우 다이어리 표시 */}
+      {selectedNoteSeq && (
+        <div className='diary-container active'>
+          <Diarycopy kidSeq={kidSelect} />
+          <button className='diary-close' onClick={() => handleDiaryClose()}>X</button>
+        </div>
+      )}
+    </div>
+
+  </div>
+  )
+}
+
+export default Note
--- sescoclient/src/page/ParentingNote/components/NoteList.jsx
+++ sescoclient/src/page/ParentingNote/components/NoteList.jsx
@@ -2,52 +2,72 @@ import React, { useEffect, useRef, useState } from 'react';
 import './noteList.css';
 import noteClose from '../noteImg/noteClose.png'
 import noteOpen from '../noteImg/noteOpen.png'
-import axios from 'axios';
+import noteKid from '../noteImg/noteKid.png'
 
-function NoteList({ notes, onNoteClick, kidSeq, allKidSeq, kids }) {
+function NoteList({ notes, onNoteClick, kidSeq, kids }) {
+  const [noteOpenStatus, setNoteOpenStatus] = useState(Array(6).fill(false));
 
-    const [noteOpenStatus, setNoteOpenStatus] = useState(Array(6).fill(false));
-    console.log("**NoteList**")
-    console.log("props로 넘겨 받은 kidSelect : ", kidSeq)
-    console.log("props로 넘겨 받은 allKidSeq : ", allKidSeq)
+  const [noteKidStatus, setNoteKidStatus] = useState(Array(6).fill(false));
+    console.log("******props*******")
+    console.log("notes : ",notes)
+    console.log("kidSeq : ",kidSeq)
+    console.log("kids : ",kids)
+    console.log("******props*******")
+    
+  const handleNoteItemClick = (index) => {
+    const newNoteOpenStatus = Array(6).fill(false);
+    newNoteOpenStatus[index] = true;
+    setNoteOpenStatus(newNoteOpenStatus);
 
-    const handleNoteItemClick = (index) => {
-        const newNoteOpenStatus = [...noteOpenStatus];
-        newNoteOpenStatus[index] = !newNoteOpenStatus[index];
-        setNoteOpenStatus(newNoteOpenStatus);
-        console.log("수첩 이미지 클릭 : ", index)
-    };
+    const newNoteKidStatus = Array(6).fill(false);
+    newNoteKidStatus[index] = true;
+    setNoteKidStatus(newNoteKidStatus);
+  };
 
-    return (
-        <div>
-            {/* 연도 div */}
-            <div className='noteList-container'>
-                <span className='noteList-year'>
+  
 
-                    {kidSeq ? kids.find((kid) => kid.kid_seq === kidSeq)?.kid_name : ""}
+  return (
+    <div>
+      {/* 연도 div */}
+      <div className='noteList-container'>
+        <span className='noteList-year'>
+          {kidSeq ? kids.find((kid) => kid.kid_seq === kidSeq)?.kid_name : ""}
+        </span>
+      </div>
 
-                </span>
+      {/* 노트 감싸는 큰 영역 */}
+      <div className="notes-container">
+        {notes.map((note, index) => (
+          <div
+            className="note-item-container"
+            key={index}
+            onClick={() => handleNoteItemClick(index)}
+            style={{
+              width: '200px',
+              height: '200px',
+              backgroundImage: noteOpenStatus[index] ? `url(${noteOpen})` : `url(${noteClose})`,
+              backgroundSize: 'cover',
+            }}
+          >
 
+             {/* 연령대 표시 */}
+             <div className='noteAge-container'>
+             <span className='noteList-age'>
+             {`${index}세`}
+            </span>
             </div>
 
-            {/* 노트 감싸는 큰 영역 */}
-            <div className="notes-container">
-                {notes.map((note, index) => (
-                    <div
-                        className="note-item-container"
-                        key={note.seq}
-                        onClick={() => handleNoteItemClick(index)}
-                        style={{
-                            width: '150px',
-                            height: '200px',
-                            backgroundImage: `url(${noteOpenStatus[index] ? noteOpen : noteClose})`,
-                            backgroundSize: 'cover',
-                        }}
-                    ></div>
-                ))}
+             {/* noteKid 이미지 표시 */}
+             <div className='noteKid-container'>
+             {noteKidStatus[index] && (
+              <img src={noteKid} alt="Note Kid" className="noteKid-image" />
+            )}
             </div>
-
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
 export default NoteList;
